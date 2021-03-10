731465afb088503814e10a807c85758aba1f20b3
Rahul Rakshit <rahul.rakshit@thoughtworks.com>
2021-03-10 12:39:58 +0100

    Expose pingpong pods through service

---
+++ pingpong_provisioning/terraform/service.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_service" "pingpong" {
+  metadata {
+    name      = var.service_name
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = var.service_name
+    }
+  }
+
+  type      = "NodePort"
+  node_port = 2000
+
+  port {
+    port        = 80
+    target_port = 1357
+  }
+
+  session_affinity = "ClientIP"
+}
