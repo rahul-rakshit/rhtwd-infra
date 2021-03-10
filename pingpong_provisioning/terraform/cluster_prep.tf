b1dfb8d5dfd84a582e99a32097bbe4d39aa5349a
Rahul Rakshit <rahul.rakshit@thoughtworks.com>
2021-03-10 13:10:57 +0100

    Try out cluster role to access port-forward

---
+++ pingpong_provisioning/terraform/cluster_prep.tf
@@ -0,0 +1,28 @@
+resource "kubernetes_cluster_role" "allow_port_forward" {
+  metadata {
+    name = "allow-port-forward"
+  }
+
+  rule {
+    verbs      = ["get", "list", "create"]
+    api_groups = [""]
+    resources  = ["pods", "pods/portforward"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "allow_port_forward" {
+  metadata {
+    name = "allow-port-forward"
+  }
+
+  subject {
+    kind = "User"
+    name = "admin"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = "allow-port-forward"
+  }
+}
+
