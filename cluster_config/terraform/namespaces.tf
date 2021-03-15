2d5927f42014c87dc86f377437175017007c60df
Rahul Rakshit <rahul.rakshit@thoughtworks.com>
2021-03-15 12:23:40 +0100

    Fix namespace definitions

--- cluster_config/terraform/namespaces.tf
+++ cluster_config/terraform/namespaces.tf
@@ -1,7 +1,11 @@
 resource "kubernetes_namespace" "staging" {
-  name = "staging"
+  metadata {
+    name = "staging"
+  }
 }
 
 resource "kubernetes_namespace" "production" {
-  name = "production"
+  metadata {
+    name = "production"
+  }
 }
