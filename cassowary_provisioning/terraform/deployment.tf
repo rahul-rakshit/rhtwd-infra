29dadd89ca64780c4f8329d60ff9f08794913591
Rahul Rakshit <rahul.rakshit@thoughtworks.com>
2021-03-15 12:53:33 +0100

    Temporarily add production cassowary deployment and service

--- cassowary_provisioning/terraform/deployment.tf
+++ cassowary_provisioning/terraform/deployment.tf
@@ -33,4 +33,41 @@ resource "kubernetes_deployment" "cassowary" {
       }
     }
   }
+
+  resource "kubernetes_deployment" "cassowary_prod" {
+    metadata {
+      name      = var.service_name
+      namespace = "production"
+
+      labels = {
+        app = var.service_name
+      }
+    }
+
+    spec {
+      replicas = 2
+      selector {
+        match_labels = {
+          app = var.service_name
+        }
+      }
+
+      template {
+        metadata {
+          labels = {
+            app = var.service_name
+          }
+        }
+        spec {
+          container {
+            image = "rahulrakshit/cassowary:latest"
+            name  = var.service_name
+            port {
+              container_port = 8080
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- cassowary_provisioning/terraform/service.tf
+++ cassowary_provisioning/terraform/service.tf
@@ -17,3 +17,23 @@ resource "kubernetes_service" "cassowary" {
     session_affinity = "ClientIP"
   }
 }
+
+resource "kubernetes_service" "cassowary_prod" {
+  metadata {
+    name      = var.service_name
+    namespace = "production"
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.cassowary_prod.metadata.0.labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+
+    session_affinity = "ClientIP"
+  }
+}
