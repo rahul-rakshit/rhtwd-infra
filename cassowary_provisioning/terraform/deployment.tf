00fddd5679e9e98b46f2fa6bc4a8319d2f7c5f28
Rahul Rakshit <rahul.rakshit@thoughtworks.com>
2021-03-15 15:33:21 +0100

    Remove cassowary resources in prod

--- cassowary_provisioning/terraform/deployment.tf
+++ cassowary_provisioning/terraform/deployment.tf
@@ -35,39 +35,3 @@ resource "kubernetes_deployment" "cassowary" {
   }
 }
 
-resource "kubernetes_deployment" "cassowary_prod" {
-  metadata {
-    name      = "${var.service_name}-prod"
-    namespace = "production"
-
-    labels = {
-      app = var.service_name
-    }
-  }
-
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = var.service_name
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = var.service_name
-        }
-      }
-      spec {
-        container {
-          image = "rahulrakshit/cassowary:latest"
-          name  = var.service_name
-          port {
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
-}
--- cassowary_provisioning/terraform/service.tf
+++ cassowary_provisioning/terraform/service.tf
@@ -18,22 +18,3 @@ resource "kubernetes_service" "cassowary" {
   }
 }
 
-resource "kubernetes_service" "cassowary_prod" {
-  metadata {
-    name      = var.service_name
-    namespace = "production"
-  }
-
-  spec {
-    selector = {
-      app = kubernetes_deployment.cassowary_prod.metadata.0.labels.app
-    }
-
-    port {
-      port        = 80
-      target_port = 8080
-    }
-
-    session_affinity = "ClientIP"
-  }
-}
