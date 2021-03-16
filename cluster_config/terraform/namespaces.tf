d936849b387c1390b98fec3a234c56d9afb46d81
Rahul Rakshit <rahul.rakshit@posteo.net>
2021-03-16 02:30:27 +0100

    Re-enable staging and prod namespaces

--- cluster_config/terraform/namespaces.tf
+++ cluster_config/terraform/namespaces.tf
@@ -1,11 +1,11 @@
-# resource "kubernetes_namespace" "staging" {
-#   metadata {
-#     name = "staging"
-#   }
-# }
+resource "kubernetes_namespace" "staging" {
+  metadata {
+    name = "staging"
+  }
+}
 
-# resource "kubernetes_namespace" "production" {
-#   metadata {
-#     name = "production"
-#   }
-# }
+resource "kubernetes_namespace" "production" {
+  metadata {
+    name = "production"
+  }
+}
