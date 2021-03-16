37db9e1adb411062c57db444bc3b65e2052892db
Rahul Rakshit <rahul.rakshit@posteo.net>
2021-03-16 02:34:55 +0100

    Re-enable cassowary deployment and service

--- cassowary_provisioning/terraform/service.tf
+++ cassowary_provisioning/terraform/service.tf
@@ -1,20 +1,20 @@
-# resource "kubernetes_service" "cassowary" {
-#   metadata {
-#     name      = var.service_name
-#     namespace = var.namespace
-#   }
+resource "kubernetes_service" "cassowary" {
+  metadata {
+    name      = var.service_name
+    namespace = var.namespace
+  }
 
-#   spec {
-#     selector = {
-#       app = kubernetes_deployment.cassowary.metadata.0.labels.app
-#     }
+  spec {
+    selector = {
+      app = kubernetes_deployment.cassowary.metadata.0.labels.app
+    }
 
-#     port {
-#       port        = 80
-#       target_port = 8080
-#     }
+    port {
+      port        = 80
+      target_port = 8080
+    }
 
-#     session_affinity = "ClientIP"
-#   }
-# }
+    session_affinity = "ClientIP"
+  }
+}
 
