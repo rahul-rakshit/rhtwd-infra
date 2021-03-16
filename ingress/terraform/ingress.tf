c77d7923f618240b3bac1c2b2ed654a3012d7773
Rahul Rakshit <rahul.rakshit@posteo.net>
2021-03-16 02:47:28 +0100

    Re-enable ingress for nginx ingress controller

--- ingress/terraform/ingress.tf
+++ ingress/terraform/ingress.tf
@@ -1,36 +1,35 @@
-# resource "kubernetes_ingress" "traefik" {
-#   metadata {
-#     name      = "traefik"
-#     namespace = var.namespace
+resource "kubernetes_ingress" "ingress" {
+  metadata {
+    name      = "ingress"
+    namespace = var.namespace
 
-#     annotations = {
-#       "kubernetes.io/ingress.class" = "traefik"
-#       "traefik.frontend.rule.type"  = "PathPrefixStrip:/staging"
-#     }
-#   }
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
 
-#   spec {
-#     rule {
-#       http {
-#         path {
-#           backend {
-#             service_name = "pingpong"
-#             service_port = 80
-#           }
+  spec {
+    rule {
+      http {
+        path {
+          backend {
+            service_name = "pingpong"
+            service_port = 80
+          }
 
-#           path = "/staging/ping"
-#         }
+          path = "ping"
+        }
 
-#         path {
-#           backend {
-#             service_name = "cassowary"
-#             service_port = 80
-#           }
+        path {
+          backend {
+            service_name = "cassowary"
+            service_port = 80
+          }
 
-#           path = "/staging/*"
-#         }
-#       }
-#     }
-#   }
-# }
+          path = "*"
+        }
+      }
+    }
+  }
+}
 
