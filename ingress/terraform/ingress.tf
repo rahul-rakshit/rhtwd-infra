a4094cf3493a6de297d205a73329468ec7d37604
Rahul Rakshit <rahul.rakshit@posteo.net>
2021-03-18 14:45:32 +0100

    Add use-regex annotation

--- ingress/terraform/ingress.tf
+++ ingress/terraform/ingress.tf
@@ -5,6 +5,7 @@ resource "kubernetes_ingress" "ingress" {
 
     annotations = {
       "kubernetes.io/ingress.class"          = "public"
+      "ingress.kubernetes.io/use-regex"      = true
       "ingress.kubernetes.io/rewrite-target" = "/$2"
     }
   }
